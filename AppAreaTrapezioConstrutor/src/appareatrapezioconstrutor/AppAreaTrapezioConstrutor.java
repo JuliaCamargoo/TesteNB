269ddea8696c6d491ced13dd05d1b128d7186268
JuliaCamargoo <Juliacs24052007@gmail.com>
2024-08-06 13:53:38 -0300

    Add files via upload

---
+++ AppAreaTrapezioConstrutor/src/appareatrapezioconstrutor/AppAreaTrapezioConstrutor.java
@@ -0,0 +1,17 @@
+
+package appareatrapezioconstrutor;
+
+import entidades.Trapezio;
+
+public class AppAreaTrapezioConstrutor {
+
+    public static void main(String[] args) {
+        double baseMaior = 10;
+        double baseMenor = 7.5;
+        double altura = 6.3;
+        
+        Trapezio x = new Trapezio(baseMaior,baseMenor,altura);
+        System.out.printf("Área X = %.2f%n", x.area());
+    }
+    
+}
---
+++ AppAreaTrapezioConstrutor/src/entidades/Trapezio.java
@@ -0,0 +1,18 @@
+
+package entidades;
+
+public class Trapezio {
+    public double baseMaior;
+    public double baseMenor;
+    public double altura;
+    
+    public Trapezio(double baseMaior, double baseMenor, double altura){
+        this.baseMaior = baseMaior;
+        this.baseMenor = baseMenor;
+        this.altura = altura;
+    }
+    
+    public double area(){
+        return (baseMaior + baseMenor) * altura /2.00;
+    }
+}
